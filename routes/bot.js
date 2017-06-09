662e9983f608612e8ee89a49964774bb1b5f5ba1
bahautdinov_bahtiyar <sabakilai@gmail.com>
2017-06-09 10:03:53 +0600

    Added Osh, Naryn, Isyk

--- routes/bot.js
+++ routes/bot.js
@@ -7,8 +7,8 @@ let xpath = require('xpath');
 let dom = require('xmldom').DOMParser;
 
 
-router.get('/meteo', function(req, res, next) {
-  request('http://meteo.kg/index.php?reg=osh', function (error, response, html) {
+router.get('/chu', function(req, res, next) {
+  request('http://meteo.kg/index.php?reg=chu', function (error, response, html) {
     if (!error && response.statusCode == 200) {
       var $ = cheerio.load(html);
       var td = [];
@@ -21,12 +21,12 @@ router.get('/meteo', function(req, res, next) {
         sunrise: td[1],
         sunset: td[3],
         radiation: td[5],
-        night_temp: td[10],
-        day_temp:td[13],
+        night_temp_pass: td[10],
+        day_temp_pass:td[13],
         night_prec:td[11],
         day_prec:td[14]
       };
-      
+
       var f_part = $('.ot').next().children().children().eq(1).text();
       var s_part = $('.ot').next().children().eq(3).text();
       var full = (f_part + s_part).replace(/(?:\r\n|\r|\n|\t)/g, "");
@@ -37,7 +37,7 @@ router.get('/meteo', function(req, res, next) {
       table[3] =  $('.sample').eq(2).children().children().eq(2).children().eq(0).text().replace(/(?:\r\n|\r|\n|\t)/g, "");
       table[4] =  $('.sample').eq(2).children().children().eq(2).children().eq(1).text().replace(/(?:\r\n|\r|\n|\t)/g, "");
       table[5] =  $('.sample').eq(2).children().children().eq(2).children().eq(2).text().replace(/(?:\r\n|\r|\n|\t)/g, "");
-      
+
       var output = {
         table1,
         table2:{
@@ -53,7 +53,7 @@ router.get('/meteo', function(req, res, next) {
           },
           text:full
         }
-        
+
       };
       res.json(output);
     } else {
@@ -65,22 +65,37 @@ router.get('/meteo', function(req, res, next) {
   });
 });
 
-router.get('/meteo2', function(req, res, next) {
-  request('http://meteo.kg/index.php?reg=chu', function (error, response, html) {
+router.get('/osh', function(req, res, next) {
+  request('http://meteo.kg/index.php?reg=osh', function (error, response, html) {
     if (!error && response.statusCode == 200) {
       var $ = cheerio.load(html);
+      var td = [];
+      $('.ot #txt table tr td').each(function (i,element) {
+        td[i] = $(this).text();
+        td[i] = td[i].replace(/(?:\r\n|\r|\n|\t)/g, "");
+      });
+      td.join(', ');
+      var table1 = {
+        night_temp_pass: td[4],
+        day_temp_pass:td[7],
+        night_prec_pass:td[5],
+        day_prec_pass:td[8]
+      };
+
       var f_part = $('.ot').next().children().children().eq(1).text();
       var s_part = $('.ot').next().children().eq(3).text();
       var full = (f_part + s_part).replace(/(?:\r\n|\r|\n|\t)/g, "");
       var table = [];
-      table[0] =  $('.sample').eq(2).children().children().eq(1).children().eq(0).text().replace(/(?:\r\n|\r|\n|\t)/g, "");
-      table[1] =  $('.sample').eq(2).children().children().eq(1).children().eq(1).text().replace(/(?:\r\n|\r|\n|\t)/g, "");
-      table[2] =  $('.sample').eq(2).children().children().eq(1).children().eq(2).text().replace(/(?:\r\n|\r|\n|\t)/g, "");
-      table[3] =  $('.sample').eq(2).children().children().eq(2).children().eq(0).text().replace(/(?:\r\n|\r|\n|\t)/g, "");
-      table[4] =  $('.sample').eq(2).children().children().eq(2).children().eq(1).text().replace(/(?:\r\n|\r|\n|\t)/g, "");
-      table[5] =  $('.sample').eq(2).children().children().eq(2).children().eq(2).text().replace(/(?:\r\n|\r|\n|\t)/g, "");
+      table[0] =  $('.sample').eq(1).children().children().eq(1).children().eq(0).text().replace(/(?:\r\n|\r|\n|\t)/g, "");
+      table[1] =  $('.sample').eq(1).children().children().eq(1).children().eq(1).text().replace(/(?:\r\n|\r|\n|\t)/g, "");
+      table[2] =  $('.sample').eq(1).children().children().eq(1).children().eq(2).text().replace(/(?:\r\n|\r|\n|\t)/g, "");
+      table[3] =  $('.sample').eq(1).children().children().eq(2).children().eq(0).text().replace(/(?:\r\n|\r|\n|\t)/g, "");
+      table[4] =  $('.sample').eq(1).children().children().eq(3).children().eq(1).text().replace(/(?:\r\n|\r|\n|\t)/g, "");
+      table[5] =  $('.sample').eq(1).children().children().eq(3).children().eq(2).text().replace(/(?:\r\n|\r|\n|\t)/g, "");
+
       var output = {
-        table:{
+        table1,
+        table2:{
           f_row:{
             name:table[0],
             night:table[1],
@@ -93,11 +108,9 @@ router.get('/meteo2', function(req, res, next) {
           },
           text:full
         }
-        
+
       };
       res.json(output);
-
-
     } else {
       return res.json({
         success:false,
@@ -107,22 +120,158 @@ router.get('/meteo2', function(req, res, next) {
   });
 });
 
-
-router.get('/meteo3', function(req, res, next) {
-  request('http://meteo.kg/index.php?reg=chu', function (error, response, html) {
+router.get('/naryn', function(req, res, next) {
+  request('http://meteo.kg/index.php?reg=naryn', function (error, response, html) {
     if (!error && response.statusCode == 200) {
+      var $ = cheerio.load(html);
+      var td = [];
+      $('.ot #txt table tr td').each(function (i,element) {
+        td[i] = $(this).text();
+        td[i] = td[i].replace(/(?:\r\n|\r|\n|\t)/g, "");
+      });
+      td.join(', ');
+      var table1 = {
+        night_temp_pass: td[4],
+        day_temp_pass:td[7],
+        night_prec_pass:td[5],
+        day_prec_pass:td[8]
+      };
+
+      var f_part = $('.ot').next().children().children().eq(1).text();
+      var s_part = $('.ot').next().children().eq(3).text();
+      var full = (f_part + s_part).replace(/(?:\r\n|\r|\n|\t)/g, "");
+      var table = [];
+      table[0] =  $('.sample').eq(1).children().children().eq(1).children().eq(0).text().replace(/(?:\r\n|\r|\n|\t)/g, "");
+      table[1] =  $('.sample').eq(1).children().children().eq(1).children().eq(1).text().replace(/(?:\r\n|\r|\n|\t)/g, "");
+      table[2] =  $('.sample').eq(1).children().children().eq(1).children().eq(2).text().replace(/(?:\r\n|\r|\n|\t)/g, "");
+      table[3] =  $('.sample').eq(1).children().children().eq(2).children().eq(0).text().replace(/(?:\r\n|\r|\n|\t)/g, "");
+      table[4] =  $('.sample').eq(1).children().children().eq(3).children().eq(1).text().replace(/(?:\r\n|\r|\n|\t)/g, "");
+      table[5] =  $('.sample').eq(1).children().children().eq(3).children().eq(2).text().replace(/(?:\r\n|\r|\n|\t)/g, "");
+
+      var output = {
+        table1,
+        table2:{
+          f_row:{
+            name:table[0],
+            night:table[1],
+            day:table[2]
+          }
+        },
+        text:full
+
+      };
+      res.json(output);
+    } else {
+      return res.json({
+        success:false,
+        message:error
+      });
+    }
+  });
+});
+
+router.get('/isyk', function(req, res, next) {
+  request('http://meteo.kg/index.php?reg=lake', function (error, response, html) {
+    if (!error && response.statusCode == 200) {
+      var $ = cheerio.load(html);
+      var td = [];
+      $('.ot #txt table tr td').each(function (i,element) {
+        td[i] = $(this).text();
+        td[i] = td[i].replace(/(?:\r\n|\r|\n|\t)/g, "");
+      });
+      td.join(', ');
+      var table1 = {
+        date:td[0],
+        radiation: td[1],
+        night_temp_pass: td[6],
+        day_temp_pass:td[9],
+        night_prec:td[7],
+        day_prec:td[10]
+      };
+
+      var f_part = $('.ot').next().children().children().eq(1).text();
+      var s_part = $('.ot').next().children().eq(2).text();
+      var full = (f_part + s_part).replace(/(?:\r\n|\r|\n|\t)/g, "");
+      var table = [];
+      table[0] =  $('.sample').eq(2).children().children().eq(1).children().eq(0).text().replace(/(?:\r\n|\r|\n|\t)/g, "");
+      table[1] =  $('.sample').eq(2).children().children().eq(1).children().eq(1).text().replace(/(?:\r\n|\r|\n|\t)/g, "");
+      table[2] =  $('.sample').eq(2).children().children().eq(1).children().eq(2).text().replace(/(?:\r\n|\r|\n|\t)/g, "");
+      table[3] =  $('.sample').eq(2).children().children().eq(2).children().eq(0).text().replace(/(?:\r\n|\r|\n|\t)/g, "");
+      table[4] =  $('.sample').eq(2).children().children().eq(2).children().eq(1).text().replace(/(?:\r\n|\r|\n|\t)/g, "");
+      table[5] =  $('.sample').eq(2).children().children().eq(2).children().eq(2).text().replace(/(?:\r\n|\r|\n|\t)/g, "");
+
+      var output = {
+        table1,
+        table2:{
+          f_row:{
+            name:table[0],
+            night:table[1],
+            day:table[2]
+          }
+        },
+        text:full
+
+      };
+      res.json(output);
+    } else {
+      return res.json({
+        success:false,
+        message:error
+      });
+    }
+  });
+});
+
+router.get('/capitals', function(req, res, next) {
+  request('http://meteo.kg/index.php?reg=capitals', function (error, response, html) {
+    if (!error && response.statusCode == 200) {
+      var $ = cheerio.load(html);
+      var td = [];
+      $('.ot #txt table tr td').each(function (i,element) {
+        td[i] = $(this).text();
+        td[i] = td[i].replace(/(?:\r\n|\r|\n|\t)/g, "");
+      });
+      td.join(', ');
+      var table1 = {
+        sunrise: td[1],
+        sunset: td[3],
+        radiation: td[5],
+        night_temp_pass: td[10],
+        day_temp_pass:td[13],
+        night_prec:td[11],
+        day_prec:td[14]
+      };
+
+      var f_part = $('.ot').next().children().children().eq(3).text().replace(/(?:\r\n|\r|\n|\t)/g, "");
+
+      var s_part = $('.ot').next().children().children().eq(7).text().replace(/(?:\r\n|\r|\n|\t)/g, "");
 
 
-      //var xml = cheerio.load(html);
-      var xml = html;
-      var doc = new dom().parseFromString(xml)
-      var nodes = xpath.select("//div", doc)
+      var table = [];
+      table[0] =  $('.sample').eq(2).children().children().eq(0).children().eq(1).text().replace(/(?:\r\n|\r|\n|\t)/g, "");
+      table[1] =  $('.sample').eq(2).children().children().eq(0).children().eq(2).text().replace(/(?:\r\n|\r|\n|\t)/g, "");
+      table[2] =  $('.sample').eq(2).children().children().eq(0).children().eq(3).text().replace(/(?:\r\n|\r|\n|\t)/g, "");
+      table[3] =  $('.sample').eq(2).children().children().eq(2).children().eq(1).text().replace(/(?:\r\n|\r|\n|\t)/g, "");
+      table[4] =  $('.sample').eq(2).children().children().eq(2).children().eq(2).text().replace(/(?:\r\n|\r|\n|\t)/g, "");
+      table[5] =  $('.sample').eq(2).children().children().eq(2).children().eq(3).text().replace(/(?:\r\n|\r|\n|\t)/g, "");
 
-      console.log(nodes[0].localName + ": " + nodes[0].firstChild.data)
-      console.log("Node: " + nodes[0].toString())
+      var output = {
+        table1,
+        table2:{
+          f_row:{
+            name:table[0],
+            night:table[1],
+            day:table[2]
+          },
+          s_row:{
+            name:table[3],
+            night:table[4],
+            day:table[5]
+          }
+        }
 
-
-
+      };
+      res.json(output. table2);
     } else {
       return res.json({
         success:false,
